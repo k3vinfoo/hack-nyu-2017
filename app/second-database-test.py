d6fca6a3f7250b3af39570be1d9e52fbf28cc3e9
Austin Vuong <vuongaustin@gmail.com>
2017-02-18 20:41:03 -0500

    Database changes

--- app/second-database-test.py
+++ app/second-database-test.py
@@ -2,9 +2,10 @@ import sqlite3 as lite
 import sys
 
 people = (
-    ('BO1234', 'hello2016', 20),
-    ('KT5678', 'hello2016', 40),
-    ('TF9101', 'hello2016', 50),
+    ('ajf500', 'hello2016', .99),
+    ('kt5678', 'hello2016', 40.00),
+    ('tf9101', 'hello2016', 50.00),
+    ('a', 'a', 10000.00),
 )
 
 con = lite.connect('people.db')
@@ -13,5 +14,5 @@ with con:
     cur = con.cursor()
 
     cur.execute("DROP TABLE IF EXISTS reps")
-    cur.execute("CREATE TABLE reps(rep_name TEXT, password TEXT, amount INT)")
+    cur.execute("CREATE TABLE reps(rep_name TEXT, password TEXT, amount REAL)")
     cur.executemany("INSERT INTO reps VALUES(?, ?, ?)", people)

1689bb30bf4a01c77861b3aea3700784e8d58cce
Austin Vuong <vuongaustin@gmail.com>
2017-02-18 19:27:32 -0500

    Database works now

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,13 @@ from flask import *
 import sqlite3
 import sys
 import os
+from os import path
 from os import listdir
 import sqlite3
 import pypyodbc
-from flask.ext.login import LoginManager, UserMixin, login_required, login_user, logout_user
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
 
-DATABASE = "C:/Users/Michelle/Desktop/GitHub/hack-nyu-2017/app/people.db"
+DATABASE = "app/people.db"
 
 app = Flask(__name__) 
 app.config.from_object(__name__)
@@ -15,15 +16,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
-
-'''
-def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-'''
+    return sqlite3.connect(DATABASE)
 
 @app.route("/login.html")
 def hello():
@@ -41,37 +34,30 @@ def search(dictionary, user):
 
 @app.route('/')
 def home():
+
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        return("You're already logged in.") ## change this to another webpage display shit/info
+        return("You're already logged in.") # change this to another webpage display shit/info
 
 @app.route('/test', methods = ['POST'])
 def user():
-    # creates database
-    g.db = connect_db()
-    cur = g.db.execute('SELECT rep_name, password, amount FROM reps')
-    data = {}
-    for row in cur.fetchall():
-        data[row[0]] = [row[1], row[2]]
-    g.db.close()
-    
-    # data/dictionary {USER: [PW, BALANCE]}
-  
-    # form verification
-    
-    #if (request.method == 'GET'):
-    #print("hello")
     user = request.form['inputNID']
     password = request.form['inputPassword']
-    #print("hello 2")
-    print(search(data,user), password, data[user][0])
-    if (search(data, user) == True and data[user][0] == password): # if there is an existing user and inputted password matches password key value for user
-        print("success!")
-    else:
-        print("Account non-existent GTFOH")
+
+    # creates database
+    g.db = connect_db()
+    query = g.db.execute('SELECT * FROM reps WHERE rep_name = ? AND password = ?', (user, password))
+    user = query.fetchone()
+
+    if user is None:
+        # Non existent
+        print("Account non-existent")
         return home(); 
-    return render_template('camera.html')
+    else:
+        # Exists
+        print("success!")
+        return render_template('camera.html')
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
